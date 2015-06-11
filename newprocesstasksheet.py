f7c21089c35fa29ef49b26df0c7178877b6406a4
Arpit Jain <arpitjain099@gmail.com>
2015-06-11 14:54:46 +1300

    user_nav working after mock1

--- newprocesstasksheet.py
+++ newprocesstasksheet.py
@@ -19,23 +19,23 @@ with open(sys.argv[1], 'rb') as csvfile:
 				time_assign=time_assign+3
 			if row[3]!="":
 				time_assign=time_assign+3
-			time_assign=time_assign+(len(row[0]).split() + len(row[4]).split() + len(row[5]).split() + len(row[6]).split() + len(row[8]).split() + len(row[9]).split())/5
+			time_assign=time_assign+(len(row[0].split()) + len(row[4].split()) + len(row[5].split()) + len(row[6].split()) + len(row[8].split())+ len(row[9].split()) )/5
 				
 			post={
-				"question":row[0],
-				"price":int(row[1]),
-				"img1url":row[2],
-				"img2url":row[3],
-				"heading":row[4],
-				"context":row[5],
-				"title":row[6],
-				"photo_upload_on":row[7],
-				"heading":row[8],
-				"text2":row[9],
-				"gps_coordinate":row[10],
-				"taskid":username+'_'+str(count),		
-				"dateofadding":str(int(time.time())),
-				"time":time_assign,
+				"question":row[0],#
+				"price":int(row[1]),#
+				"img1url":row[2],#
+				"img2url":row[3],#
+				"heading":row[4],#
+				"context":row[5],#
+				"title":row[6],#
+				"photo_upload_on":row[7],#
+				"text1":row[8],#
+				"text2":row[9],#
+				"gps_coordinate":row[10],##
+				"taskid":username+'_'+str(count),	#	
+				"dateofadding":str(int(time.time())),#
+				"time":time_assign,#
 				}
 			collection.insert(post)
 		post = collection_rec.find_one({"username":username})
